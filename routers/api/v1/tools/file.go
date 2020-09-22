853c8667cb17e17b74e028f84d676ff9c7250668
xingchen.lin <lxc175322@alibaba-inc.com>
2020-09-22 10:23:29 +0800

    fin upload file

--- routers/api/v1/tools/file.go
+++ routers/api/v1/tools/file.go
@@ -4,9 +4,14 @@ import (
 	"FrontEndOJGolang/pkg/app"
 	"FrontEndOJGolang/pkg/e"
 	"FrontEndOJGolang/pkg/setting"
+	"encoding/hex"
 	"github.com/gin-gonic/gin"
 	"log"
 	"net/http"
+	"os"
+	"path"
+	"crypto/sha1"
+	"strconv"
 	"strings"
 )
 
@@ -15,29 +20,58 @@ func UploadFile(c *gin.Context) {
 	appG := app.Gin{
 		C: c,
 	}
+
+	userSession, err := app.GetUserFromSession(c.Request)
+	if err != nil {
+		log.Printf("[ERROR] get user session error[%v]\n", err)
+		appG.Response(http.StatusBadRequest, e.INVALID_PARAMS, nil)
+		return
+	}
+
 	fileReader, err := c.FormFile("file")
 	if err != nil {
 		log.Printf("[ERROR] upload file error:[%v]", err)
 		appG.Response(http.StatusOK, e.INVALID_PARAMS, "upload file error")
 		return
 	}
-	err = c.SaveUploadedFile(fileReader, setting.ToolSetting.FileBaseDir+"/"+fileReader.Filename)
+
+	// gen new hashed filename
+	h := sha1.New()
+	h.Write([]byte(fileReader.Filename))
+	fileName := hex.EncodeToString(h.Sum(nil)) + path.Ext(fileReader.Filename)
+
+	filePath := setting.ToolSetting.FileBaseDir+"/"+strconv.FormatUint(userSession.Id, 10)
+	os.MkdirAll(filePath, os.ModePerm)
+	filePath += "/"+fileName
+
+	err = c.SaveUploadedFile(fileReader, filePath)
 	if err != nil {
 		log.Printf("[ERROR] save upload file error:[%v]", err)
 		appG.Response(http.StatusOK, e.INVALID_PARAMS, "save upload file error")
 		return
 	}
-	appG.Response(http.StatusOK, e.SUCCESS, fileReader.Filename)
+	appG.Response(http.StatusOK, e.SUCCESS, fileName)
 }
 
 func GetFile(c *gin.Context) {
 	appG := app.Gin{
 		C: c,
 	}
+
+	userSession, err := app.GetUserFromSession(c.Request)
+	if err != nil {
+		log.Printf("[ERROR] get user session error[%v]\n", err)
+		appG.Response(http.StatusBadRequest, e.INVALID_PARAMS, nil)
+		return
+	}
+
+
 	file := c.Query("file")
 	if file == "" || strings.Contains(file, "..") || strings.Contains(file, "/") {
 		appG.Response(http.StatusOK, e.INVALID_PARAMS, "invalid file url")
 		return
 	}
-	c.File(setting.ToolSetting.FileBaseDir + "/" + file)
+
+	filePath := setting.ToolSetting.FileBaseDir+"/"+strconv.FormatUint(userSession.Id, 10)+"/"+file
+	c.File(filePath)
 }
